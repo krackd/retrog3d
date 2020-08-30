6a9157169643781c4cd3cdd2b45c8fbfd576c08b
krackd <43828895+krackd@users.noreply.github.com>
2020-08-30 20:15:29 +0200

    Initial

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    public UiObjectName UiObjectName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+
+            GameObject go = hit.transform.gameObject;
+            DisplayObjectName(go);
+        }
+    }
+
+    private void DisplayObjectName(GameObject go)
+    {
+        Game game = go.GetComponent<Game>();
+        UiObjectName.gameObject.SetActive(game != null);
+
+        if (game != null)
+        {
+            UiObjectName.ObjectName = game.GameName;
+        }
+    }
+}
---
+++ Assets/UiObjectName.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiObjectName : MonoBehaviour
+{
+
+    public TMPro.TextMeshProUGUI Label;
+
+    public string ObjectName {
+        set {
+            Label.text = value;
+        }
+
+        get {
+            return Label.text;
+        }
+    }
+
+}
